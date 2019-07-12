6ed5f82e86c781a191690c1ab246556e59f0c885
Mark Raymond <mark.raymond@red-gate.com>
2019-07-12 13:59:18 +0100

    Trivial class library.

---
+++ MyClassLibrary/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public static class MyClass
+    {
+        public static double Tetrate(double a, int n)
+        {
+            if (n < 0)
+            {
+                throw new NotImplementedException("Negative tetration not implemented");
+            }
+
+            if (n == 0)
+            {
+                return 1.0;
+            }
+
+            var result = a;
+            var logA = Math.Log(a);
+
+            n--;
+            while (n > 0)
+            {
+                result = Math.Exp(logA * result);
+                n--;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MyClassLibraryTests/MyClassTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MyClassLibrary
+{
+    [TestFixture]
+    public class MyClassTests
+    {
+        [Test]
+        [TestCase(1.0, 0, 1.0)]
+        [TestCase(1.0, 1, 1.0)]
+        [TestCase(1.0, 2, 1.0)]
+        [TestCase(2.0, 0, 1.0)]
+        [TestCase(2.0, 1, 2.0)]
+        [TestCase(2.0, 2, 4.0)]
+        [TestCase(2.0, 3, 16.0)]
+        [TestCase(2.0, 4, 65536.0)]
+        [TestCase(3.0, 0, 1.0)]
+        [TestCase(3.0, 1, 3.0)]
+        [TestCase(3.0, 2, 27.0)]
+        [TestCase(3.0, 3, 7625597484987.0)]
+        [TestCase(4.0, 0, 1.0)]
+        [TestCase(4.0, 1, 4.0)]
+        [TestCase(4.0, 2, 256.0)]
+        [TestCase(4.0, 3, 1.3407807929942597e154)]
+        public void TetrationTests(double a, int n, double expected)
+        {
+            const double epsilon = 0.0000000000001;
+
+            var actual = MyClass.Tetrate(a, n);
+            var difference = Math.Abs((actual / expected) - 1);
+            Assert.That(difference, Is.LessThan(epsilon));
+        }
+    }
+}
