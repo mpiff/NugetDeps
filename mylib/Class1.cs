40cfe1b5870f2396cd26acd938dd647a52f4aee2
PIFF Markus <markus.piff@leica-geosystems.com>
2020-10-22 10:10:00 +0200

    add additional dep

---
+++ mylib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace mylib
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
