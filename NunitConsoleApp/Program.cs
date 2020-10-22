b2329273f05377275556fec39e54d81374b2c973
PIFF Markus <markus.piff@leica-geosystems.com>
2020-10-22 09:25:35 +0200

    added deps

--- NunitConsoleApp/Program.cs
+++ NunitConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace NunitConsoleApp
     {
         static void Main(string[] args)
         {
+
+            MyClass.Tetrate(1d, 2);
+            var test = new MyClassTests();
+            test.TetrationTests(1d, 2, 1d);
         }
     }
 }
